17a8c1e7c44caecf9e8d7241f696b807f9d0e44a
Guillermo Elgorriaga <guillermo.elgorriaga@arkusinc.com>
2020-01-03 17:04:52 -0300

    wip

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -53,6 +53,11 @@ public class Main {
     return "index";
   }
 
+  @RequestMapping("/test")
+  String test() {
+    return "test";
+  }
+
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {
